6adc56c32ff55a75129669b9d22915c3ddf07bb4
Chris Jenkins <cdpjenkins@gmail.com>
2020-06-29 13:36:38 +0100

    Lesson 2.27: Make boxes clickable and multi-coloured

--- app/src/main/java/com/cdpjenkins/android/colourmyviews/MainActivity.kt
+++ app/src/main/java/com/cdpjenkins/android/colourmyviews/MainActivity.kt
@@ -1,12 +1,40 @@
 package com.cdpjenkins.android.colourmyviews
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setListeners()
     }
+
+    private fun setListeners() {
+        val clickableView =
+            listOf(box_one_text, box_two_text, box_three_text, box_four_text, box_five_text, constraint_layout)
+
+        for (item in clickableView) {
+            item.setOnClickListener { makeColoured(it) }
+        }
+    }
+
+    private fun makeColoured(view: View) {
+        when (view.id) {
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.DKGRAY)
+
+            R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
+    }
+
+
 }
